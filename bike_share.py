a73aa4dbf8697a3f61de7fb819eb2cc9acc703a8
Vishaka Balasubramanian Sekar <vishakabsekar@gmail.com>
2017-07-25 09:22:38 -0700

    revamped the code, added normalization, plotting, changed regression logic

--- bike_share.py
+++ bike_share.py
@@ -12,7 +12,7 @@ y_test_datum = []#[[] for _ in range(400)]
 with open('./data_set/day.csv', 'r') as days:
     days_reader = csv.DictReader(days)
     for row in days_reader:
-        data = row['season'], row['yr']
+        data = row['temp'], row['yr']
         count_data =  row['cnt'],row['yr']
 
         if data[1] == '0':
@@ -36,7 +36,7 @@ training_datum_array = training_datum_array.astype(np.float)
 y_training_datum_array = np.asarray(y_training_datum)
 y_training_datum_array = y_training_datum_array.astype(np.float)
 
-# print(training_datum_array.shape)
+#print(training_datum_array)
 # print(test_datum_array.shape)
 # print(training_datum_array)
 # print(test_datum_array)
@@ -48,24 +48,59 @@ test_datum_array = test_datum_array.astype(np.float)
 y_test_datum_array = np.asarray(y_test_datum)
 y_test_datum_array = y_test_datum_array.astype(np.float)
 
+# print("dzgsgs")
+# print(training_datum_array[:,0])
 
-nn = Regressor(layers=[Layer("Rectifier", units=100), Layer("Linear")]
-                , warning=None, parameters=None, random_state=None, learning_rule=u'sgd', learning_rate=0.5,
-               learning_momentum=0.9, normalize=None, regularize=None, weight_decay=None, dropout_rate=None,
-               batch_size=1, n_iter=None, n_stable=10, f_stable=0.001, valid_set=None, valid_size=0.0, loss_type=None,
-               callback=None, debug=False, verbose=None)
+nn = Regressor(
+    layers=[
+        Layer("Sigmoid", units=100),
+        Layer("Linear")],
+    learning_rate=0.001,
+    n_iter=2000)
 
-nn.fit(training_datum_array, y_training_datum_array)
+# test_datum_array = test_datum_array[test_datum_array[:, 1].argsort()]
+# y_test_datum_array = y_test_datum_array[y_test_datum_array[:,1].argsort()]
+
+training_seasons = np.asarray(training_datum_array[:,0])
+training_counts = np.asarray(y_training_datum_array[:,0])
 
 
-y_output = nn.predict(test_datum_array)
+training_seasons = np.array([[training_season] for training_season in training_seasons])
+training_counts = np.array([[training_count] for training_count in training_counts])
+
+
+training_seasons = training_seasons - np.mean(training_seasons)/(max(training_seasons)- min(training_seasons))
+training_counts = training_counts- np.mean(training_counts)/(max(training_counts)-min(training_counts))
+
+print(training_seasons)
+nn.fit(training_seasons, training_counts)
+
+
+# test_datum_array = test_datum_array[test_datum_array[:, 1].argsort()]
+# y_test_datum_array = y_test_datum_array[y_test_datum_array[:,1].argsort()]
+
+testing_seasons  = np.asarray(test_datum_array[:,0])
+testing_counts = np.asarray(y_test_datum_array[:,0])
+
+
+
+testing_seasons = np.array([[testing_season] for testing_season in testing_seasons])
+testing_counts  = np.array([[testing_count] for testing_count in testing_counts])
+
+testing_seasons = testing_seasons - np.mean(testing_seasons)/(max(testing_seasons)- min(testing_seasons))
+testing_counts = testing_counts - np.mean(testing_counts)/(max(testing_counts)-min(testing_counts))
+
+
+
+y_output = nn.predict(testing_seasons)
 print(y_output)
-
+#
 print("\n\n")
+#
+print(testing_counts)
 
-print(y_test_datum_array)
-
-# plt.plot(test_datum_array[0], y_output[0])
-# plt.show();
-# plt.plot(test_datum_array[0], y_test_datum_array[0])
-# plt.show();
+plt.plot(testing_seasons, testing_counts,'ro')
+plt.show()
+y_output = y_output*(max(testing_counts)-min(testing_counts))+np.mean(testing_counts)
+plt.plot(testing_seasons, y_output,'ro')
+plt.show()
