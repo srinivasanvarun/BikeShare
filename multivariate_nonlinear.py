7473ce827bf5e1a459a88b05403fc6bb4c302735
Dhruv Mevada <mevadadhruv90@gmail.com>
2017-07-29 13:54:57 -0700

    update multivariate to plot correctly and change regression layers

--- multivariate_nonlinear.py
+++ multivariate_nonlinear.py
@@ -1,3 +1,8 @@
+all = [var for var in globals() if (var[:2], var[-2:]) != ("__", "__")]
+for var in all:
+    del globals()[var]
+
+
 from sknn.mlp import Regressor, Layer
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,12 +19,18 @@ def print_array_shapes(arrays):
         print('\n\n')
         print(array.shape)
 
-def create_np_array(array, index):
+def standardize(array, index):
     base_array = np.asarray(array[:, index])
     base_array = np.array([[elements] for elements in base_array])
-    base_array = base_array - np.mean(base_array) / (max(base_array) - min(base_array))
+    base_array = (base_array - np.mean(base_array)) / (max(base_array) - min(base_array))
     return base_array
 
+
+def destandardize(array, index):
+    base_array = np.asarray(array[:, index])
+    base_array = np.array([[elements] for elements in base_array])
+    base_array = (base_array * (max(base_array) - min(base_array))) + np.mean(base_array)
+    return base_array
 
 def parse_data():
     with open('./data_set/day.csv', 'r') as days:
@@ -56,63 +67,52 @@ def parse_data():
 
 training_datum, y_training_datum, test_datum, y_test_datum = parse_data()
 
-# arrays = [training_datum, y_training_datum, test_datum, y_test_datum]
-# print_array(arrays)
-
-# training_datum = np.array([[elements] for elements in training_datum])
-
-training_atemp = create_np_array(training_datum, 0)
-training_hum = create_np_array(training_datum, 1)
-training_ws = create_np_array(training_datum, 2)
-
-# arrays = [training_atemp, training_hum, training_ws]
-# print_array(arrays)
-# print_array_shapes(arrays)
-
-training_counts = create_np_array(y_training_datum, 0)
+training_atemp = standardize(training_datum, 0)
+training_hum = standardize(training_datum, 1)
+training_ws = standardize(training_datum, 2)
+training_counts = standardize(y_training_datum, 0)
 
 nn = Regressor(
     layers=[
-        Layer("Sigmoid", units=100),
-        Layer("Linear")],
-    learning_rate=0.005,
-    n_iter=3500)
+        Layer("Sigmoid", units = 50),
+        Layer("Linear", units = 5),
+        Layer("Linear", units = 1)],
+    learning_rate=0.000005,
+    n_iter=2000)
 
-nn.fit(training_datum[:,:2], y_training_datum[:,0])
-# nn.fit(training_hum, training_counts)
-# nn.fit(training_ws, training_counts)
 
-testing_atemp = create_np_array(test_datum, 0)
-testing_hum = create_np_array(test_datum, 1)
-testing_ws = create_np_array(test_datum, 2)
-testing_counts = create_np_array(y_test_datum, 0)
-#
-# arrays = [testing_atemp, testing_hum, testing_ws]
-# print_array(arrays)
-# print_array_shapes(arrays)
-#
-# y_output = nn.predict(testing_atemp, testing_hum, testing_ws)
+# nn = Regressor( # Vishaka- reason for change -https://www.researchgate.net/post/why_the_prediction_or_the_output_of_neural_network_does_not_change_during_the_test_phase
+#     layers=[
+#         Layer("Sigmoid", units=50), #28 was optimum,
+#         Layer("Linear", units = 5),#5
+#        # Layer("Linear", units = 4),#5 was optimum
+#         Layer("Linear", units = 1 )],
+#     learning_rate=0.0000005,
+#     n_iter=2000)
 
-y_output = nn.predict(test_datum[:,:2])
+nn.fit(training_datum[:,:3], y_training_datum[:,0])
 
-# y_output_1 = nn.predict(testing_hum)
-# y_output_2 = nn.predict(testing_ws)
-#
-y_output =  y_output * (max(testing_counts) - min(testing_counts)) + np.mean(testing_counts)
-# y_output_1 = y_output_1 * (max(testing_counts) - min(testing_counts)) + np.mean(testing_counts)
-# y_output_2 = y_output_2 * (max(testing_counts) - min(testing_counts)) + np.mean(testing_counts)
-#
-fig, axis = plt.subplots(figsize=(3, 3))
-axis.set_title('Bike Count'.format('seaborn'), color = 'green')
+testing_atemp = standardize(test_datum, 0)
+testing_hum = standardize(test_datum, 1)
+testing_ws = standardize(test_datum, 2)
+testing_counts = standardize(y_test_datum, 0)
+
+y_output = nn.predict(test_datum[:,:3])
+
+predicted_output = destandardize(y_output, 0)
+actual_ouput = destandardize(y_test_datum, 0)
+
+testing_atemp = destandardize(testing_atemp, 0)
+
+# fig, axis = plt.subplots(figsize=(6, 6))
+# axis.set_title('Bike Count'.format('seaborn'), color = 'green')
 #
 # plt.plot(testing_atemp, testing_counts, 'ro', color='blue', label ='Test data')
-plt.plot(testing_atemp, y_output, 'ro', label ='Prediction')
+
+plt.xlabel('Average Temperature')
+plt.ylabel('Count of Bikes Rented')
+plt.suptitle('Multivariate Non Linear Regression')
+
+plt.plot(testing_atemp, actual_ouput, 'ro')
+plt.plot(testing_atemp, predicted_output, 'bo')
 plt.show()
-#
-# plt.plot(testing_hum, testing_counts, 'ro', color='blue', label = 'Test data 2')
-# plt.plot(testing_hum, y_output_1, 'ro', label='Prediction')
-# plt.show()
-#
-# plt.plot(testing_ws, testing_counts, 'ro', color='blue', label = 'Test data 3')
-# plt.plot(testing_ws, y_output_2, 'ro', label='Prediction')
-# plt.show()
